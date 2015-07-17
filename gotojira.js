a9cd4126af490aba84160c00a688c08a5e4740f3
Matita <matitald@gmail.com>
2015-07-17 11:24:51 +0200

    added alert for unknown projects

--- gotojira.js
+++ gotojira.js
@@ -13,13 +13,18 @@ javascript:(function(){
 		text = prompt('Write the JIRA identifier (e.g. "662" or "MAN-662")');
 	
 	if (text) {
-		var m = text.match(/((\w+)-+)?(\d+)/);
+		var m = text.match(/((\w+)-*)?(\d+)/);
 		if (!m)
 			return alert('Pattern not matched (e.g. MAN-662)');
-		var proj = (m[1] || defaultProj).toUpperCase();
+		
+		var proj = (m[2] || defaultProj).toUpperCase();
 		var id = m[3];
 		var jiraId = proj + '-' + id;
-		var baseUrl = []
+		var baseUrl = baseUrls[proj];
+
+		if (!baseUrl)
+			return alert('No url found for project "' + proj + '"');
+
 		var url = baseUrl + (baseUrl[baseUrl.length - 1] == '/' ? '' : '/') + 'browse/' + jiraId;
 		window.open(url);
 	}
